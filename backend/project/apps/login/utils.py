d4288a3ad3cfd2b82d5b73f27ce83c326bf4d943
Jani Aaltonen <N5728@student.jamk.fi>
2020-07-17 13:10:31 +0300

    custom response to auth/signup

--- backend/project/apps/login/utils.py
+++ backend/project/apps/login/utils.py
@@ -1,7 +1,10 @@
 import datetime
 import jwt
 from django.conf import settings
-
+from rest_framework.response import Response
+from rest_framework import exceptions
+from django.contrib.auth import authenticate
+import datetime
 
 def generate_access_token(user):
 
@@ -23,3 +26,28 @@ def generate_refresh_token(user):
     }
     refresh_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256').decode('utf-8')
     return refresh_token
+
+class CustomResponse:
+
+    def __init__(self, username, password):
+        self.response = Response()
+        if username is None or password is None:
+            raise exceptions.AuthenticationFailed
+        self.user = authenticate(username=username, password=password)
+        if self.user is None:
+            raise exceptions.AuthenticationFailed('wrong username or password')
+        self.generate_tokens()
+
+    def generate_tokens(self):
+        access_token_age = datetime.timedelta.total_seconds(datetime.timedelta(hours=0, minutes=5))
+        access_token = generate_access_token(self.user)
+        refresh_token_age = datetime.timedelta.total_seconds(datetime.timedelta(days=1))
+        refresh_token = generate_refresh_token(self.user)
+        self.response.set_cookie(key='refresh_token', value=refresh_token, httponly=True, max_age=refresh_token_age)
+        self.response.set_cookie(key='access_token', value=access_token, httponly=True, max_age=access_token_age)
+
+    def set_data(self, data):
+        self.response.data = data
+
+
+
--- backend/project/apps/login/views.py
+++ backend/project/apps/login/views.py
@@ -2,10 +2,11 @@ from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import exceptions, permissions, authentication
-from .utils import generate_access_token, generate_refresh_token
+from .utils import generate_access_token, generate_refresh_token, CustomResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 import datetime
+from django.contrib.auth.forms import UserCreationForm
 
 
 class AuthView(APIView):
@@ -17,19 +18,23 @@ class AuthView(APIView):
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
-        response = Response()
-        if (username is None) or password is None:
-            raise exceptions.AuthenticationFailed()
-        user = authenticate(username=username, password=password)
-        if user is None:
-            raise exceptions.AuthenticationFailed('wrong username or password')
-        access_token_age = datetime.timedelta.total_seconds(datetime.timedelta(hours=0, minutes=5))
-        access_token = generate_access_token(user)
-        refresh_token_age = datetime.timedelta.total_seconds(datetime.timedelta(days=1))
-        refresh_token = generate_refresh_token(user)
-        response.set_cookie(key='refresh_token', value=refresh_token, httponly=True, max_age=refresh_token_age)
-        response.set_cookie(key='access_token', value=access_token, httponly=True, max_age=access_token_age)
-        response.data = {
-            'detail': 'login successful'
-        }
-        return response
+        cr = CustomResponse(username, password)
+        cr.set_data({'detail': 'login successful'})
+
+        return cr.response
+
+
+class SignUp(APIView):
+
+    authentication_classes = []
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+
+
