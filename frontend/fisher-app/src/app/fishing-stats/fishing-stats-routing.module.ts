d64188629e020dde0d5259e7a6951049e9aafb57
Jani Aaltonen <N5728@student.jamk.fi>
2020-09-18 13:20:11 +0300

    url changed for edit view

--- frontend/fisher-app/src/app/fishing-stats/fishing-stats-routing.module.ts
+++ frontend/fisher-app/src/app/fishing-stats/fishing-stats-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
       { path: '', component: ListComponent },
       { path: 'add', component: AddComponent },
       { path: 'details/:id', component: DetailsComponent },
-      { path: 'details/:id/edit', component: EditComponent }
+      { path: 'details/:id/edit', component: EditComponent },
+      { path: 'details/:id/methods/:methodIndex/edit', component: EditComponent}
     ]
   }
 ];
