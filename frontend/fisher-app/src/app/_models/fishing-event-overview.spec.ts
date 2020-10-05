c96ebb9c62a9c4ce409cf0420361b805b27d9ef6
Jani Aaltonen <N5728@student.jamk.fi>
2020-10-05 21:19:25 +0300

    model for new fishingevent

---
+++ frontend/fisher-app/src/app/_models/fishing-event-overview.spec.ts
@@ -0,0 +1,7 @@
+import { FishingEventOverview } from './fishing-event-overview';
+
+describe('FishingEventOverview', () => {
+  it('should create an instance', () => {
+    expect(new FishingEventOverview()).toBeTruthy();
+  });
+});
---
+++ frontend/fisher-app/src/app/_models/fishing-event-overview.ts
@@ -0,0 +1,12 @@
+export class FishingEventOverview {
+  id: number;
+  date: string;
+  location: string;
+  location_details: string;
+  start_time: string;
+  end_time: string;
+  weather: string;
+  air_temperature: number;
+  persons: number;
+  total_catches: number;
+}
