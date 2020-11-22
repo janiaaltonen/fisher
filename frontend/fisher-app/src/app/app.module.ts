91a69fda80fb3286ea336bc5df1709986d46b300
Jani Aaltonen <N5728@student.jamk.fi>
2020-11-22 16:40:11 +0200

    custom validator added

--- frontend/fisher-app/src/app/app.module.ts
+++ frontend/fisher-app/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
 import { LoginComponent } from './login/login.component';
-import { ErrorInterceptor, TokenInterceptor } from '@app/_helpers';
+import {CustomValidators, ErrorInterceptor, TokenInterceptor} from '@app/_helpers';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
@@ -23,7 +23,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: CustomValidators, useClass: SignUpComponent}
   ],
   bootstrap: [AppComponent]
 })
