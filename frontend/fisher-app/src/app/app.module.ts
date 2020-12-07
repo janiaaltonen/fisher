85d325fa9fa9584a736c6d0cd4cb5286a2957a28
Jani <jani.aaltonen@outlook.com>
2020-12-07 20:55:19 +0200

    imports added

--- frontend/fisher-app/src/app/app.module.ts
+++ frontend/fisher-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
@@ -8,15 +8,20 @@ import {ReactiveFormsModule} from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import {CustomValidators, ErrorInterceptor, TokenInterceptor} from '@app/_helpers';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { CustomMaterialModule } from '@app/custom-material/custom-material.module';
+import { ConfirmDialogComponent } from './_components/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule
@@ -26,6 +31,6 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     {provide: CustomValidators, useClass: SignUpComponent}
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
